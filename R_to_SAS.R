b1a56bd4573bb082538ba71baa41c80af60dd185
benson-wu <bensonwu@Bensons-MBP.lan>
2021-11-11 20:13:14 -0800

    update

--- R_to_SAS.R
+++ R_to_SAS.R
@@ -1,14 +1,12 @@
 library(foreign)
 female <- read.csv("/Users/bensonwu/Documents/UCLA/2021-2022/BIOSTAT_203A/Project1/BIOSTAT-203A/LAFEMA01_1.csv")
-female$sex <- 0
-female$sex <- factor(female$sex)
+female$sex <- "Female"
 write.foreign(female, "/Users/bensonwu/Documents/UCLA/2021-2022/BIOSTAT_203A/Project1/BIOSTAT-203A/female.csv",
               "/Users/bensonwu/Documents/UCLA/2021-2022/BIOSTAT_203A/Project1/BIOSTAT-203A/female.sas", package="SAS")
 
 
 
-male <- read.csv("/Users/bensonwu/Documents/UCLA/2021-2022/BIOSTAT_203A/Project1/BIOSTAT-203A/LAFEMA01_1.csv")
-male$sex <- 1
-male$sex <- factor(male$sex)
+male <- read.csv("/Users/bensonwu/Documents/UCLA/2021-2022/BIOSTAT_203A/Project1/BIOSTAT-203A/LAMENA03_1.csv")
+male$sex <- "Male"
 write.foreign(male, "/Users/bensonwu/Documents/UCLA/2021-2022/BIOSTAT_203A/Project1/BIOSTAT-203A/male.csv",
               "/Users/bensonwu/Documents/UCLA/2021-2022/BIOSTAT_203A/Project1/BIOSTAT-203A/male.sas", package="SAS")
